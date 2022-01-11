49c856771e12a84b921ca8cccc181f6acc3ab84b
menphis232 <menphisj@gmail.com>
2022-01-11 18:56:58 -0400

    first commit cambios a capacitor

---
+++ src/app/app.module.ts
@@ -0,0 +1,64 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { Globals } from './app.globals';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Facebook } from '@ionic-native/facebook/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { OptionPageModule } from './option/option.module';
+import { OfferPageModule } from './offer/offer.module';
+import { PayPal } from '@ionic-native/paypal/ngx';
+import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { Stripe } from '@ionic-native/stripe/ngx';
+import { NativeGeocoder} from '@ionic-native/native-geocoder/ngx';
+
+import { SignInWithApple, AppleSignInResponse, AppleSignInErrorResponse, ASAuthorizationAppleIDRequest } from '@ionic-native/sign-in-with-apple/ngx';
+
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot(
+        {
+            backButtonText: 'Retour'
+        }
+    ),
+    AppRoutingModule,
+    HttpClientModule,
+    OptionPageModule,
+    OfferPageModule,
+    AngularFireModule.initializeApp(environment.config),
+    AngularFireAuthModule
+  ],
+  providers: [
+    Globals,
+    Geolocation,
+    NativeGeocoder,
+    PayPal,
+
+    OneSignal,
+    Stripe,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    GooglePlus,
+    Facebook,
+    SignInWithApple
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
